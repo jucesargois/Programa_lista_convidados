439a10a684abe9e68f4c665eb909bae030cd880d
Julio <jucesargois@gmail.com>
2022-01-05 15:32:34 -0300

    Created new project
    Program: Lista de convidados

---
+++ test/programa_lista_convidados_test.dart
@@ -0,0 +1,8 @@
+import 'package:programa_lista_convidados/programa_lista_convidados.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
