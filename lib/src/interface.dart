b0cb62a3672bb8de24159438c95d039dd1fa84c3
Julio <jucesargois@gmail.com>
2022-01-05 17:46:52 -0300

    Update interface.dart

--- lib/src/interface.dart
+++ lib/src/interface.dart
@@ -1,16 +1,44 @@
-import 'package:programa_lista_convidados/src/lista.dart';
+import 'dart:io';
 
-class HomeInterface extends Lista {
-  HomeInterface() : super(convidados: []);
-}
-
-Lista lista = Lista(convidados: []);
 
 HomeInterfaceView(bool condicao, opcoes, int tam) {
-  switch (opcoes) {
-    case "Y":
-      print('');
-      break;
-    default:
+  String? resp;
+  print('Bem-vindo gostária de criar uma lista de convidados? Y/N.');
+  resp = stdin.readLineSync();
+  if (resp == "Y") {
+    MenuOp();
+  } else {
+    print("===========PROGRAMA FINALIZADO==============");
   }
+}
+
+MenuOp() {
+  String? menuresp;
+  String? nomelista;
+  int? tamlist;
+  List<String> convidados = [];
+
+  print('Qual será o nome da sua lista');
+  nomelista = stdin.readLineSync();
+  print("=========== NOME DA LISTA: $nomelista ==============");
+  print("Quantos convidados gostária de adicionar a lista? ");
+  tamlist = int.parse(stdin.readLineSync()!);
+  print("LISTA PARA: $nomelista, COM $tamlist CONVIDADOS");
+
+  print("Favor digitar abaixo os nomes dos convidados.");
+  //nomelista = stdin.readLineSync();
+
+  for (var i = 0; i <= tamlist; i++) {
+    nomelista = stdin.readLineSync();
+    convidados.add(nomelista as String);
+  }
+
+  print("Gostária de gerar sua lista em um arquivo de texto?");
+ 
+
+  //print(convidados); printa lista
+  //DEIXAR VERIFICAÇÃO DO MENU PARA O FINAL.
+  // if (nomelista == "") {
+  // print("Digite um nome válido");
+  //}
 }
