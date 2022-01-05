489a38e966a29221fb4ea24742bd064620dd7175
Julio <jucesargois@gmail.com>
2022-01-05 15:39:25 -0300

    Delete programa_lista_convidados.dart

--- bin/programa_lista_convidados.dart
+++
@@ -1,5 +0,0 @@
-import 'package:programa_lista_convidados/programa_lista_convidados.dart' as programa_lista_convidados;
-
-void main(List<String> arguments) {
-  print('Hello world: ${programa_lista_convidados.calculate()}!');
-}
