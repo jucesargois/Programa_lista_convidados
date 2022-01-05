532b5c51c7d8d10f0a3892fed3356e423e35a867
Julio <jucesargois@gmail.com>
2022-01-05 17:46:45 -0300

    Update main.dart
    Inserted call for HomeInterfaceView and created new variables

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,10 @@
-void main(){
-  
+import 'dart:io';
+
+import 'package:programa_lista_convidados/src/interface.dart';
+String? opcoes;
+bool condicao = true;
+ main(List<String> arguments) {
+  String resp;
+  HomeInterfaceView(condicao,opcoes,5);
+  opcoes = stdin.readLineSync();
 }
