c3cd0fbd2439aa83d0b61d5e2ebd399a8022f7a7
Julio <jucesargois@gmail.com>
2022-01-26 14:13:21 -0300

    Update main.dart
    Remove unnecessary things

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,5 @@
-import 'dart:io';
-
 import 'package:programa_lista_convidados/src/interface.dart';
-String? opcoes;
-bool condicao = true;
- main(List<String> arguments) {
-  String resp;
-  HomeInterfaceView(condicao,opcoes,5);
-  opcoes = stdin.readLineSync();
+
+main(List<String> arguments) {
+  homeInterfaceView();
 }
